12bf3bff8439bb0f69e176e6f8e7a1cf33b815ab
DaseulChun <nandaseul@gmail.com>
2020-01-23 00:33:50 -0800

    add a UserList Page/Component

--- src/user/pages/Users.js
+++ src/user/pages/Users.js
@@ -1,7 +1,18 @@
 import React from 'react';
 
+import UsersList from '../components/UsersList';
+
 const Users = () => {
-  return <h2>Users Works!</h2>;
+  const USERS = [
+    {
+      id: 'u1', 
+      name: 'Max Schwarz',
+      image: 'https://randomuser.me/api/portraits/men/62.jpg',
+      places: 3
+    }
+  ];
+
+  return <UsersList items={USERS} />;
 }
 
 export default Users;
