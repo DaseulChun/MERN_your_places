f2c0190205ce7e1a8a7d5d2249c4c77a19bbc155
DaseulChun <nandaseul@gmail.com>
2020-01-24 15:46:37 -0800

    animate the SideDrawer

--- src/shared/components/Navigation/NavLinks.js
+++ src/shared/components/Navigation/NavLinks.js
@@ -1,13 +1,15 @@
-import React from 'react';
-import { NavLink } from 'react-router-dom';
+import React from "react";
+import { NavLink } from "react-router-dom";
 
-import './NavLinks.css';
+import "./NavLinks.css";
 
 const NavLinks = props => {
   return (
     <ul className="nav-links">
       <li>
-        <NavLink to="/" exact>ALL USERS</NavLink>
+        <NavLink to="/" exact>
+          ALL USERS
+        </NavLink>
       </li>
       <li>
         <NavLink to="/u1/places">MY PLACES</NavLink>
@@ -19,7 +21,7 @@ const NavLinks = props => {
         <NavLink to="/auth">AUTHENTICATE</NavLink>
       </li>
     </ul>
-  )
+  );
 };
 
 export default NavLinks;
