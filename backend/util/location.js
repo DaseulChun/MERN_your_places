315bd202a8d203979eac8c2191570286e4dbfa4f
DaseulChun <nandaseul@gmail.com>
2020-03-26 14:57:59 -0700

    add creating and storing the Place into the DB

--- backend/util/location.js
+++ backend/util/location.js
@@ -1,8 +1,9 @@
 const axios = require("axios");
+require("dotenv").config();
 
 const HttpError = require("../models/http-error");
 
-const API_KEY = "YOUR API KEY";
+const API_KEY = `${process.env.GOOGLE_GEOCODING_API_KEY}`;
 
 async function getCoordsForAddress(address) {
   const response = await axios.get(
