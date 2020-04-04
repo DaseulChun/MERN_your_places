bb4f1def204bf62a66217853931ba32fd7c07fe9
DaseulChun <nandaseul@gmail.com>
2020-04-04 13:52:20 -0700

    cleaned code

--- client/src/places/pages/NewPlace.js
+++ client/src/places/pages/NewPlace.js
@@ -8,7 +8,7 @@ import LoadingSpinner from "../../shared/components/UIElements/LoadingSpinner";
 import ImageUpload from "../../shared/components/FormElements/ImageUpload";
 import {
   VALIDATOR_REQUIRE,
-  VALIDATOR_MINLENGTH
+  VALIDATOR_MINLENGTH,
 } from "../../shared/util/validators";
 import { useForm } from "../../shared/hooks/form-hook";
 import { useHttpClient } from "../../shared/hooks/http-hook";
@@ -22,27 +22,27 @@ const NewPlace = () => {
     {
       title: {
         value: "",
-        isValid: false
+        isValid: false,
       },
       description: {
         value: "",
-        isValid: false
+        isValid: false,
       },
       address: {
         value: "",
-        isValid: false
+        isValid: false,
       },
       image: {
         value: null,
-        isValid: false
-      }
+        isValid: false,
+      },
     },
     false
   );
 
   const history = useHistory();
 
-  const placeSubmitHandler = async event => {
+  const placeSubmitHandler = async (event) => {
     event.preventDefault();
 
     try {
@@ -50,11 +50,10 @@ const NewPlace = () => {
       formData.append("title", formState.inputs.title.value);
       formData.append("description", formState.inputs.description.value);
       formData.append("address", formState.inputs.address.value);
-      formData.append("creator", auth.userId);
       formData.append("image", formState.inputs.image.value);
 
       await sendRequest("http://localhost:5000/api/places", "POST", formData, {
-        Authorization: "Bearer " + auth.token
+        Authorization: "Bearer " + auth.token,
       });
       history.push("/");
     } catch (err) {}
